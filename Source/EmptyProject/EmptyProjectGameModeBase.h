3665b45bbc96ac0c254eda8fddebf1927e212c08
Sergey <Sergeev_sergey99@mail.ru>
2019-02-12 22:14:33 +0300

    second commit

---
+++ Source/EmptyProject/EmptyProjectGameModeBase.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EmptyProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMPTYPROJECT_API AEmptyProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+public:
+	AEmptyProjectGameModeBase();
+	
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
